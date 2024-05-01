6b461b4e708ef966ddb887a44c8bf14df1702c12
CodeZeki <zeki76k@gmail.com>
2024-05-01 16:41:29 +0200

    rtztzrhft

--- EventProjectApi/Controllers/Data/ApplicationDbContext.cs
+++ EventProjectApi/Controllers/Data/ApplicationDbContext.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EventProjectApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 
 
 namespace EventProjectApi.Controllers.Data
 {
-	public class ApplicationDbContext : IdentityDbContext
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 	{
 		public ApplicationDbContext(DbContextOptions options)
 			: base(options)
 		{
 		}
+		public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+		public DbSet<EventItem> EventItems { get; set; }
+
 	}
 }
