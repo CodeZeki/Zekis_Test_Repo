913e99b0d04d43910f330114e76903bf11b72647
robgit21 <79367605+robgit21@users.noreply.github.com>
2024-05-01 14:38:38 +0200

    add update

--- EventProjectApi/obj/Debug/net8.0/EventProjectApi.AssemblyInfo.cs
+++ EventProjectApi/obj/Debug/net8.0/EventProjectApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventProjectApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f52feecd1b6689aa6a1f680a248d082fc93a14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+244423af6b0b4fea87222e216a2df0e666084533")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventProjectApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventProjectApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
