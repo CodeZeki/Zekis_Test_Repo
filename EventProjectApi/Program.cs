244423af6b0b4fea87222e216a2df0e666084533
CodeZeki <zeki76k@gmail.com>
2024-05-01 14:33:20 +0200

    add database

--- EventProjectApi/Program.cs
+++ EventProjectApi/Program.cs
@@ -1,6 +1,13 @@
 
+using EventProjectApi.Controllers.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace EventProjectApi
 {
+    
+    //pipeline !!
+    
     public class Program
     {
         public static void Main(string[] args)
@@ -8,16 +15,20 @@ namespace EventProjectApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>   //   Registriert er Db Context 
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDbConnection")); // Datenbank Verbindung
+            });
+            builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle   //UI Seite
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            var app = builder.Build();                    // Aufbau von Service
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())          // Entwicklungsmodus aoll sich die UI öffnen (Button benutzen kannst über die Seite)
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
